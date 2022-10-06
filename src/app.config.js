34e5a3821b249a99a5718238f8ff8221b4c22024
rmadhumanchi <rmadhumanchi@deloitte.com>
2022-10-06 16:19:11 +0000

    Initial commit

---
+++ src/app.config.js
@@ -0,0 +1,10 @@
+/**
+ * Place to store data specific to this instance
+ * DO NOT store secrets here
+ * DO NOT store state here - must be part of a stateless service
+ */
+class AppConfig {
+    team = 'awsuser39';
+    version = '2.0.0';
+}
+module.exports = AppConfig;
